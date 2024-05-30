edeba364ea5959df609b31e863a17682fddfd888
Bahkmat <161296043+Bahkmat@users.noreply.github.com>
2024-05-30 14:33:49 +0500

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnitConverter unit = new UnitConverter();
+            Console.WriteLine("Выберите куда перевести: \n Для перевода из метров в футы нажмите 1 \n Для перевода из метров в дюймы напишите 2 \n Для перевода из футов в метры нажмите 3 \n Для перевода из футов в дюймы нажмите 4 \n Для перевода из дюймов в метры нажмите 5 \n Для перевода из дюймов в футы нажмите 6");
+            Console.WriteLine("\n Для перевода из КГ В ФТ напишите 7 \n Для перевода из КГ В Уц напишите 8 \n Для перевода из ФТ В КГ напишите 9 \n Для перевода из ФТ В УЦ напишите 10 \n Для перевода из УЦ В КГ  напишите 11 \n Для перевода из УЦ В фт напишите 12 \n");
+            Console.WriteLine(" Для перевода из литры В галлоны напишите 13 \n Для перевода из литров В куб метры напишите 14 \n Для перевода из галлонов В литры напишите 15 \n Для перевода из галлонов В Куб метры напишите 16 \n Для перевода из куб метров В литры  напишите 17 \n Для перевода из галлонов В литры напишите 18 \n");
+            Console.WriteLine("Для перевода из цельсий в фарингейт 19 \n Для перевода из фарингейта в цельсию 20");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите единицу измерения");
+            double a = double.Parse(Console.ReadLine());
+            switch (c)
+            {
+                case 1:
+                    unit.add1(a);
+                    break;
+                case 2:
+                    unit.add2(a);
+                    break;
+                case 3:
+                    unit.add3(a);
+                    break;
+                case 4:
+                    unit.add4(a);
+                    break;
+                case 5:
+                    unit.add5(a);
+                    break;
+                case 6:
+                    unit.add6(a);
+                    break;
+                case 7:
+                    unit.add77(a);
+                    break;
+                case 8:
+                    unit.add8(a);
+
+                    break;
+                case 9:
+                    unit.add9(a);
+                    break;
+                case 10:
+                    unit.add10(a);
+
+                    break;
+                case 11:
+                    unit.add11(a);
+                    break;
+                case 12:
+                    unit.add12(a);
+                    break;
+                case 13:
+                    unit.add13(a);
+                    break;
+                case 14:
+                    unit.add14(a);
+                    break;
+                case 15:
+                    unit.add15(a);
+                    break;
+                case 16:
+                    unit.add16(a);
+                    break;
+                case 17:
+                    unit.add17(a);
+                    break;
+                case 18:
+                    unit.add18(a);
+                    break;
+                case 19:
+                    unit.add19(a);
+                    break;
+                case 20:
+                    unit.add20(a);
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp10/ConsoleApp10/UnitConverter.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class UnitConverter
+    {
+        public void add1 (double a) //Метр в фут
+        {
+            double Fout = a * 3.28084;
+            Console.WriteLine("КГ В Футах" + Fout.ToString());
+        } 
+        public void add2(double a) //Метров в дюйм
+        {
+            double Duims = a * 39.37;
+            Console.WriteLine("Метров в Дюймах: " + Duims.ToString());
+        }
+       
+        public void add3(double a) //Фута в метр
+        { 
+            double Metr = a * 0.3048;
+            Console.WriteLine("Футов в Метрах: " + Metr.ToString());
+        } 
+        public void add4(double a) //Фута в дюйм
+        {
+            double Duims = a * 12;
+            Console.WriteLine("Футов в Дюймах: " + Duims.ToString());
+        }
+        public void add5(double a) //Дюйм в метры
+        {
+            double Metrs = a * 0.0254;
+            Console.WriteLine("Дюймов в Метрах: " + Metrs.ToString());
+        }
+        public void add6(double a) // Дюйма в фут
+        {
+            double Duims = a / 12;
+            Console.WriteLine("дюймов в футах:" + Duims.ToString());
+        }
+        public void add77(double a) //КГ В ФТ
+             
+        {
+            double Duims = a * 2.20462;
+            Console.WriteLine("Фунты:" + Duims.ToString());
+        }
+        public void add8(double a) // КГ В Унции
+        {
+            double Duims = a * 35.2739619;
+            Console.WriteLine("Фунты:" + Duims.ToString());
+        }
+        public void add9(double a) // КГ В Унции
+        {
+            double Duims = a * 0.453592;
+            Console.WriteLine("Фунты:" + Duims.ToString());
+        }
+        public void add10(double a) 
+        {
+            double Duims = a * 12;
+            Console.WriteLine("Дюймов: " + Duims.ToString());
+        }
+        public void add11(double a)
+        {
+            double Duims = a * 12;
+            Console.WriteLine("Дюймов: " + Duims.ToString());
+        }
+        public void add12(double a)
+        {
+            double Duims = a * 12;
+            Console.WriteLine("Дюймов: " + Duims.ToString());
+        }
+
+        public void add13(double a) 
+        {
+            double Metrs = a * 0.0254;
+            Console.WriteLine("Дюймов в Метрах: " + Metrs.ToString());
+
+        }
+        public void add14(double a) 
+        {
+            double Metrs = a * 0.0254;
+            Console.WriteLine("Дюймов в Метрах: " + Metrs.ToString());
+        }
+        public void add15(double a)
+        { 
+            double Metrs = a * 0.0254;
+            Console.WriteLine("Дюймов в Метрах: " + Metrs.ToString());
+        }
+        public void add16(double a) 
+        {
+            double Metrs = a * 0.0254;
+            Console.WriteLine("Дюймов в Метрах: " + Metrs.ToString());
+        }
+        public void add17(double a)
+        {
+            double Metrs = a * 0.0254;
+            Console.WriteLine("Дюймов в Метрах: " + Metrs.ToString());
+        }
+        public void add18(double a) 
+        {
+            double Metrs = a * 0.0254;
+            Console.WriteLine("Дюймов в Метрах: " + Metrs.ToString());
+        }
+        public void add19(double a) 
+        {
+            double Duims = a * 39.37;
+            Console.WriteLine("Метров в Дюймах: " + Duims.ToString());
+        }
+        public void add20(double a)
+        {
+            double Duims = a - 32 * 5/9;
+            Console.WriteLine("из фарингейта в цельсию: " + Duims.ToString());
+        }
+
+    }
+}
